759018b8a26eb35e0371e9c24bb7b48b08189113
Lennert Vanden Herrewegen <lennert.vanden.herrewegen@student.ehb.be>
2017-05-23 09:44:56 +0200

    JSON bestand toegevoegd

--- js/SearchPage.js
+++ js/SearchPage.js
@@ -3,6 +3,18 @@ __https://stackoverflow.com/questions/9012537/how-to-get-the-element-clicked-for
 
 
 */
+var schilderijen;
+var zoekWaarde;
+$(function () {
+  $.ajax({
+    url: '../JSON/Bedrijven.json'
+    , dataType: 'json'
+    , success: onSuccess
+    , error: function (err) {
+      console.error('Fout: ', err);
+    }
+  });
+});
 $(function () {});
 
 function ToggleFilters(e) {
@@ -17,5 +29,23 @@ function ToggleFilters(e) {
     listItems.id = ("notToggled");
     var spanItem = $("#notToggled span")
     spanItem.width(0 + "%");
+  }
+}
+/*---____---____---___---____---____---___---____---____---___---____---____---___---____---____---___---____---____---___---____---____---___---____---____---___---____---____---___*/
+function zoekWaardeVeranderen() {
+  zoekWaarde = $("#search").val();
+  genereerLijstMetBedrijven();
+}
+
+function zoek() {
+  if (zoekWaarde != null) {
+    var filterArray = [];
+    schilderijen.forEach(function (schilderij) {
+      var zoekRegex = new RegExp(zoekWaarde, "gi");
+      if (schilderij.naam.match(zoekRegex)) {
+        filterArray.push(schilderij)
+      }
+    });
+    schilderijen = filterArray;
   }
 }
