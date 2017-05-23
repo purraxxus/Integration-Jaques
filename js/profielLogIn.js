6cf500d52d4b1618de928ea1d00b35dfec256c60
Aron Raes <aron.raes@student.ehb.be>
2017-05-23 11:26:43 +0200

    f

--- js/profielLogIn.js
+++ js/profielLogIn.js
@@ -1,31 +1,40 @@
-function dropdownUsername (){
-    
-    $("#username").css("margin-top","5%").css("transition", "1s");
-    
+function dropdownUsername() {
+
+    $("#username").css("margin-top", "5%").css("transition", "1s").css("height", "100%");
+
 }
 
-function dropdownPassword (){
-    
-    $("#password").css("margin-top","5%").css("transition", "1s");
-    
+function dropdownPassword() {
+
+    $("#password").css("margin-top", "5%").css("transition", "1s").css("height", "100%");
+
 }
 
-function clickOutUsername (){
+function clickOutUsername() {
     var input = document.getElementById("username").value;
-    if (input == "" || input.length == 0 || input == null)
-{
-     $("#username").css("margin-top","0%");
-    }else{
-}
+    if (input == "" || input.length == 0 || input == null) {
+        $("#username").css("margin-top", "0%");
+    } else {}
 }
 
-function clickOutPassword (){
+function clickOutPassword() {
     var input = document.getElementById("password").value;
-    if (input == "" || input.length == 0 || input == null)
-{
-     $("#password").css("margin-top","0%");
-    }else{
-        
+    if (input == "" || input.length == 0 || input == null) {
+        $("#password").css("margin-top", "0%");
+    } else {
+
     }
 }
 
+function send() {
+    var user = document.getElementById("username").value;
+
+    var pass = document.getElementById("password").value;
+
+    if (user == "jos" && pass == 1234) {
+        window.location.assign("/html/profiel.html")
+    }
+
+    console.log(user);
+    console.log(pass);
+}
