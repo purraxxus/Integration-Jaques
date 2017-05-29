76a5a553b5191809f5bcb9426d5988ec4785dd9d
Aron Raes <aron.raes@student.ehb.be>
2017-05-29 11:47:38 +0200

    js events

--- js/event_detailpage.js
+++ js/event_detailpage.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-    console.log("oke");
 
 
     $.getJSON("../JSON/events.json", function (json) //json bestand inladen
@@ -9,36 +8,27 @@ $(document).ready(function () {
             
                 var deze = json.evenementen;
                 var data = "";
-        console.log(deze);
+        
 
                 data += '<p class="company_name_event">' + deze[0].datum + '</p>'
                 data +='<p>'+deze[0].uur+'</p>'
-        
-//console.log(data);
+    
               
- $('#data').append(data);
+                $('#data').append(data);
 
        
-
-           
-          
-
-
-
                 var plaats = "";
 
                 plaats +=  '<p class="company_name_event">' + deze[0].naamEvent + '</p>'
                     
-                plaats += '<p>'  + deze[0].staatNummer + ' </p>'
+                plaats += '<p>'  + deze[0].straatNummer + ' </p>'
                 
-                plaast += '<p>' + deze[0].data + '</p>'
+                plaats += '<p>' + deze[0].stad + '</p>'
         
                 plaats += '<p><a href="mailto:info@kittybons.com">contact</a></p>'
       
         
-
-             console.log(plaats);
- $('#plaats').append(plaats);
+                $('#plaats').append(plaats);
 
        
 
