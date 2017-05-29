615b31f5da65d2e4d40fcbe7e8d9cf4f6d27f698
KB2016\Kitty Bons <kitty.bons@student.ehb.be>
2017-05-29 10:42:58 +0200

    added counter

--- js/buttons_subscribe.js
+++ js/buttons_subscribe.js
@@ -1,14 +1,21 @@
 //Dit is het hartje voor subscribers
-
 $(document).ready(function () {
   $(".connect_users").click(function () {
-    $("i#hartje").css("color", "red").text("event");
-    $("p").text()
+    $("i#hartje").css("color", "red").text("favorite");
+    $(".connect_users p").html(function (i, val) {
+      return +val + 1
+    });
+    });
+
+  
+//
+//
+//  Dit is het labeltje voor attend
+  $(".attend_link").click(function () {
+    $("i#attend_label").css("color", "green").text("label");
+    $(".attend_link p").text("You're in!")
   });
 });
-
-
-
 //$("p").click(function(){
 //    $("#number_subs").text(function(i, origText){
 //        return origText + "1" (index: " i + ")";
