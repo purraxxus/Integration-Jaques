d49519196157e3524802a3128842f886eefe5061
KB2016\Kitty Bons <kitty.bons@student.ehb.be>
2017-05-29 09:34:54 +0200

    hartje gefixd

--- js/buttons_subscribe.js
+++ js/buttons_subscribe.js
@@ -1,12 +1,16 @@
-$(document).ready(function(){
-  $(".....").click(function(){
-    $("test").html("insert text");
-    
-    
-    
+//Dit is het hartje voor subscribers
+
+$(document).ready(function () {
+  $(".connect_users").click(function () {
+    $("i#hartje").css("color", "red").text("event");
+    $("p").text()
   });
-  
-  
-  
-  
 });
+
+
+
+//$("p").click(function(){
+//    $("#number_subs").text(function(i, origText){
+//        return origText + "1" (index: " i + ")";
+//    });
+//});
