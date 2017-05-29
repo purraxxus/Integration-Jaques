633ca1a792a42f1e685cdf61cc8eeccd88a75215
Lennert Vanden Herrewegen <lennert.vanden.herrewegen@student.ehb.be>
2017-05-29 12:18:25 +0200

    gwnwaorde

--- js/SearchPage.js
+++ js/SearchPage.js
@@ -22,8 +22,29 @@ $(function () {
 function onSuccess(data) {
   Bedrijven = data.features;
 }
+//-_-_-_--_-_-_--_-_-_--_-_-_--_-_-_ZOEK--_-_-_--_-_-_--_-_-_--_-_-_--_-ZOEK-_--_-_-_--_-_-_--_-_-_--_-_-_--_-_-_--_-_-_--_ZOEK_-_--_-_-_--_-_-_--_-_-_--_-_-_ZOEK_-_-_--_-_-_--_-_-_--_-_-_--_-_-_--_-_-_-
+function zoekWaardeVeranderen() //Verander de zoekwaarde bij het typen
+{
+  zoekWaarde = $("#search").val();
+  genereerLijstMetBedrijven();
+}
 
-function ToggleFilters(e) {
+function zoek() //Maak een regex aan met de zoekwaarden en Zoek
+{
+  if (zoekWaarde != null) {
+    var filterArray = [];
+    newBedrijven.forEach(function (newBedrijven) {
+      var zoekRegex = new RegExp(zoekWaarde, "gi");
+      if (newBedrijven.properties.Naam.match(zoekRegex)) {
+        filterArray.push(newBedrijven)
+      }
+    });
+    newBedrijven = filterArray;
+  }
+}
+//-_-_-_--_-_-_--_-_-_--_-_-_--_-_-Filter--_-_-_--_-_-_--_-_-_--_-_-_--_-Filter-_--_-_-_--_-_-_--_-_-_--_-_-_--_-_-_--_-_-_--Filter-_--_-_-_--_-_-_--_-_-_--_-_-Filter-_-_--_-_-_--_-_-_--_-_-_--_-_-_--_-_-_-
+function ToggleFilters(e) //verander filters en past aan bij filters tijdens search
+{
   var listItems = e.target;
   var listItemsText = $(listItems).text();
   var andereListItems = $("#filterMenu li")
@@ -45,45 +66,9 @@ function ToggleFilters(e) {
   }
   Filterwaardes();
 }
-/*---____---____---___---____---____---___---____---____---___---____---____---___---____---____---___---____---____---___---____---____---___---____---____---___---____---____---___*/
-function zoekWaardeVeranderen() {
-  zoekWaarde = $("#search").val();
-  genereerLijstMetBedrijven();
-}
 
-function zoek() {
-  if (zoekWaarde != null) {
-    var filterArray = [];
-    newBedrijven.forEach(function (newBedrijven) {
-      var zoekRegex = new RegExp(zoekWaarde, "gi");
-      if (newBedrijven.properties.Naam.match(zoekRegex)) {
-        filterArray.push(newBedrijven)
-      }
-    });
-    newBedrijven = filterArray;
-  }
-}
-
-function genereerLijstMetBedrijven() {
-  newBedrijven = Bedrijven;
-  Filter();
-  zoek();
-  FilterFunctie();
-  var htmlString = "";
-  if (newBedrijven.length) {
-    for (i = 0; i < newBedrijven.length; i++) {
-      htmlString += '<div id="' + i + '" class="Company"><img class="Search" src="' + newBedrijven[i].properties.source + '">'
-      htmlString += '<h3 class="Search">' + newBedrijven[i].properties.Naam + '</h3></div>'
-      cssChanger(i);
-      $("#content").html(htmlString);
-    }
-  }
-  else {
-    $('main#content').empty();
-  }
-}
-
-function Filterwaardes() {
+function Filterwaardes() // genereer string voor regex bij filter
+{
   FilterwaardeArray = "";
   var filters = $(".toggled");
   for (i = 0; i < filters.length; i++) {
@@ -101,7 +86,8 @@ function Filterwaardes() {
   Filterwaarde = FilterwaardeArray;
 }
 
-function Filter() {
+function Filter() //pas regex toe en filter 
+{
   if (Filterwaarde != null) {
     var filterArray = [];
     Bedrijven.forEach(function (Bedrijven) {
@@ -113,22 +99,44 @@ function Filter() {
     newBedrijven = filterArray;
   }
 }
-
-function cssChanger(nr) {
-  if (nr % 2 != 0) {
-    var classDing = $(nr + ".Company");
-    $("#" + nr).css("background-color", "yellow");
-  }
-}
-
-function FilterFunctie() {
+//-_-_-_--_-_-_--_-_-_--_-_-_--_-_-Bij search--_-_-_--_-_-_--_-_-_--_-_-_--_-Bij search-_--_-_-_--_-_-_--_-_-_--_-_-_--_-_-_--_-_-_--Bij search-_--_-_-_--_-_-_--_-_-_--_-_-Bij search-_-_--_-_-_--_-_-_--_-_-_--_-_-_--_-_-_-
+function FilterFunctie() //pas Header aan bij zoeken 
+{
   var searchBalk = $("#search");
   var FilterKnop = $("header img")
   searchBalk.css("width", "75%");
   FilterKnop.css("height", "30px");
 }
 
-function ToonFilterMenu() {
+function ToonFilterMenu() //Toon het meny voor te filteren 
+{
   var FilterMenu = $("#filterMenu");
   FilterMenu.slideToggle();
 }
+//-_-_-_--_-_-_--_-_-_--_-_-_--_-_-HTML vullen--_-_-_--_-_-_--_-_-_--_-_-_--_-HTML vullen-_--_-_-_--_-_-_--_-_-_--_-_-_--_-_-_--_-_-_--HTML vullen-_--_-_-_--_-_-_--_-_-_--_-_-HTML vullen-_-_--_-_-_--_-_-_--_-_-_--_-_-_--_-_-_-
+function genereerLijstMetBedrijven() // Pas filters en zoek toe en genereer lijst in html
+{
+  newBedrijven = Bedrijven;
+  Filter();
+  zoek();
+  FilterFunctie();
+  var htmlString = "";
+  if (newBedrijven.length) {
+    for (i = 0; i < newBedrijven.length; i++) {
+      htmlString += '<div id="' + i + '" class="Company"><img class="Search" src="' + newBedrijven[i].properties.source + '">'
+      htmlString += '<h3 class="Search">' + newBedrijven[i].properties.Naam + '</h3></div>'
+      cssChanger(i);
+      $("#content").html(htmlString);
+    }
+  }
+  else {
+    $('main#content').empty();
+  }
+}
+/*function cssChanger(nr) 
+{
+if (nr % 2 != 0) {
+  var classDing = $(nr + ".Company");
+  $("#" + nr).css("background-color", "yellow");
+}
+}*/
