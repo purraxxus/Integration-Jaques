c3edc07c0b0fdb75fa066a29ce386ddd824d79cb
Aron Raes <aron.raes@student.ehb.be>
2017-05-30 09:55:20 +0200

    json blba

--- js/event_detailpage.js
+++ js/event_detailpage.js
@@ -6,8 +6,22 @@ $(document).ready(function () {
         {
 
             var deze = json.evenementen;
-            var data = "";
 
+
+
+            var titelEvent = "";
+
+            titelEvent += '<h1 id="naam_company">' + deze[0].naam + ' </h1>'
+
+
+            $('#naamEvent').append(titelEvent);
+
+
+
+
+
+
+            var data = "";
 
             data += '<p class="company_name_event">' + deze[0].datum + '</p>'
             data += '<p>' + deze[0].uur + '</p>'
@@ -33,79 +47,79 @@ $(document).ready(function () {
             var otherEvents = "";
 
             otherEvents += '<div class = "example_item_title" > ' + deze[0].naam + ' </div>'
-            
-            otherEvents += '<div class = "example_item_description" > '+deze[0].details+'</div>'
- 
-             $('#otherEvent').append(otherEvents);
-        
-            
-            
-        
-            
-             var otherEvent1 = "";
-            
+
+            otherEvents += '<div class = "example_item_description" > ' + deze[0].details + '</div>'
+
+            $('#otherEvent').append(otherEvents);
+
+
+
+
+
+            var otherEvent1 = "";
+
             otherEvent1 += '<div class = "example_item_title" > ' + deze[1].naam + ' </div>'
-            
-            otherEvent1 += '<div class = "example_item_description" > '+deze[1].details+'</div>'
+
+            otherEvent1 += '<div class = "example_item_description" > ' + deze[1].details + '</div>'
 
             $('#otherEvent1').append(otherEvent1);
 
-            
-        
-        
-        
-        
-            
+
+
+
+
+
+
             var otherEventdatum = "";
-            
-            otherEventdatum += '<span  class="example_item_title_on_image">'+deze[0].datum+'</span>'
+
+            otherEventdatum += '<span  class="example_item_title_on_image">' + deze[0].datum + '</span>'
 
             $('#otherEventDatum').append(otherEventdatum);
 
-        
-        
-        
-        
+
+
+
+
             var otherEventdatum1 = "";
-            
-            otherEventdatum1 += '<span  class="example_item_title_on_image">'+deze[1].datum+'</span>'
+
+            otherEventdatum1 += '<span  class="example_item_title_on_image">' + deze[1].datum + '</span>'
 
             $('#otherEventDatum1').append(otherEventdatum1);
-            
-               
 
 
-        
+
+
+
             var jobsEvent = "";
-            
-            jobsEvent += '<div class="example_item_date"> <span class="example_item_title_on_image">'+deze[0].jobs[0].fuctie+'</span></div> <div class="example_item_title">'+deze[0].jobs[0].welkeJob+' </div> <div class="example_item_description">'+deze[0].jobs[0].beschrijving+'</div>'
+
+            jobsEvent += '<div class="example_item_date"> <span class="example_item_title_on_image">' + deze[0].jobs[0].fuctie + '</span></div> <div class="example_item_title">' + deze[0].jobs[0].welkeJob + ' </div> <div class="example_item_description">' + deze[0].jobs[0].beschrijving + '</div>'
 
             $('#job1').append(jobsEvent);
-        
-        
-        
-        
-            
+
+
+
+
+
             var jobs1Event = "";
-            
-            jobs1Event += '<div class="example_item_date"> <span class="example_item_title_on_image">'+deze[0].jobs[1].fuctie+'</span></div> <div class="example_item_title">'+deze[0].jobs[1].welkeJob+' </div> <div class="example_item_description">'+deze[0].jobs[1].beschrijving+'</div>'
+
+            jobs1Event += '<div class="example_item_date"> <span class="example_item_title_on_image">' + deze[0].jobs[1].fuctie + '</span></div> <div class="example_item_title">' + deze[0].jobs[1].welkeJob + ' </div> <div class="example_item_description">' + deze[0].jobs[1].beschrijving + '</div>'
 
             $('#job2').append(jobs1Event);
-        
-        
-        
-        
-            
+
+
+
+
+
             var reviewEvent = "";
-            
-            reviewEvent += '<p>'+deze[0].review+'</p>'
+
+            reviewEvent += '<p>' + deze[0].review + '</p>'
 
             $('#review').append(reviewEvent);
-        
-        
-        
-        
-        
+
+
+
+
+
         });
 });
 
