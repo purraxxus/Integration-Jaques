f386e022682673330eb8322f98cccf7db9eba2dd
Lennert Vanden Herrewegen <lennert.vanden.herrewegen@student.ehb.be>
2017-05-30 10:53:00 +0200

    Anas Months arrows work

--- js/calender.js
+++ js/calender.js
@@ -1,51 +1,35 @@
 $(document).ready(function () {
-    console.log("oke");
-
-
-    $.getJSON("../JSON/events.json", function (json) //upload json file
-        {
-    
-            console.log("test");
-            $.each(json.evenementen, function () {
-                var details = "";
-
-                details += '<div class="entry">';
-                details += ' <img src="' + this.url +'" alt="" class="event-img" />';
-
-                details += '  <div class="text">';
-                details += '    <span class="title">' + this.naamEvent + '<span class="' + this.naam + '"></span></span>';
-                details += '    <span class="time">' + this.uur + '</span>';
-                details += '   </div>';
-                details += ' </div>';
-
-              
- $('.entry-container').append(details);
-
-            });
-
-           });
-
-// add 7 days to each day in the calendar    
-
+  $.getJSON("../JSON/events.json", function (json) //upload json file
+    {
+      console.log("test");
+      $.each(json.evenementen, function () {
+        var details = "";
+        details += '<div class="entry">';
+        details += ' <img src="' + this.url + '" alt="" class="event-img" />';
+        details += '  <div class="text">';
+        details += '    <span class="title">' + this.naamEvent + '<span class="' + this.naam + '"></span></span>';
+        details += '    <span class="time">' + this.uur + '</span>';
+        details += '   </div>';
+        details += ' </div>';
+        $('.entry-container').append(details);
+      });
+    });
+  // add 7 days to each day in the calendar    
   $(".next-day").click(function () {
     $(".day span").html(function (i, a) {
-        return +a + 7;
-        
-        
-   });
-      
-      });
-    
-
-
+      return +a + 7;
+    });
+  });
   $(".prev-day").click(function () {
     $(".day span").html(function (i, a) {
       return +a - 7
-      
     });
-    });
-
-    
-    
+  });
 });
+var OnPageMonth = 4;
 
+function Months(i) {
+  var Months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  OnPageMonth += i;
+  $(".month").text(Months[Math.abs(OnPageMonth % 12)]);
+}
