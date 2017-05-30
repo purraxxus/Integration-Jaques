f6b54a1f7dfd6c1a1b03458ca59c325d3e6d4c17
purraxxus <thatcronosdude@outlook.com>
2017-05-30 16:20:18 +0200

    link

--- js/getDataForMap.js
+++ js/getDataForMap.js
@@ -65,7 +65,7 @@ function loadMap(map, txtinput) {
                     var adres = data.features[i].properties.Adres;
                     var latLng = new google.maps.LatLng(coords[1], coords[0]);
 
-                    var contentString = "<a style='text-decoration: none'; color: black><h1>" + name + "<h1><br>" + "<p>" + descr + "<p><br>" + "<p>" + adres + "<p><br></a>";
+                    var contentString = "<a style='text-decoration: none; color: black' href='Event_Detailpage.html'><h1>" + name + "<h1><br>" + "<p>" + descr + "<p><br>" + "<p>" + adres + "<p><br></a>";
                     var infowindow = new google.maps.InfoWindow({
                         content: contentString
                     });
@@ -83,9 +83,6 @@ function loadMap(map, txtinput) {
                         infowindow.open(map, this);
                         window.localStorage.setItem('bedrijfsNaam',this.bedrijf);
                         console.log(this.bedrijf);
-                    });
-                    google.maps.event.addListener(infowindow, 'click', function(){
-                        window.location=('Event_Detailpage.html');
                     });
                 }
             },
@@ -107,7 +104,7 @@ function loadMap(map, txtinput) {
                     var time = data.evenementen[i].uur;
                     var adres = data.evenementen[i].straatNummer + " " + data.evenementen[i].stad;
                     var latLng = new google.maps.LatLng(coords[1], coords[0]);
-                    var contentString = "<a style='text-decoration: none; color: black'><h1>" + naam + "<h1><br>" + "<p>" + descr + "<p style='color: lightgrey;><br>" + time + "<p><br>" + "<p>" + adres + "<p><br></a>";
+                    var contentString = "<a style='text-decoration: none; color: black' href='Event_Detailpage.html'><h1>" + naam + "<h1><br>" + "<p>" + descr + "<p style='color: lightgrey;><br>" + time + "<p><br>" + "<p>" + adres + "<p><br></a>";
                     var infowindow = new google.maps.InfoWindow({
                         content: contentString
                     });
@@ -125,9 +122,6 @@ function loadMap(map, txtinput) {
                         infowindow.open(map, this);
                         window.localStorage.setItem('eventNaam',this.bedrijf);
                         console.log(this.bedrijf);
-                    });
-                    google.maps.event.addListener(infowindow, 'click', function(){
-                        window.location=('Event_Detailpage.html');
                     });
                 }
             },
@@ -151,7 +145,7 @@ function loadMap(map, txtinput) {
                     var adres = data.evenementen[i].straatNummer + " " + data.evenementen[i].stad;
                     var latLng = new google.maps.LatLng(coords[1], coords[0]);
 
-                    var contentString = "<a style='text-decoration: none; color: black'><h1>" + naam + "<h1><br>" + "<p>" + descr + "<p style='color: lightgrey;><br>" + time + "<p><br>" + "<p>" + adres + "<p><br></a>";
+                    var contentString = "<a style='text-decoration: none; color: black' href='Event_Detailpage.html'><h1>" + naam + "<h1><br>" + "<p>" + descr + "<p style='color: lightgrey;><br>" + time + "<p><br>" + "<p>" + adres + "<p><br></a>";
                     var infowindow = new google.maps.InfoWindow();
                     var marker = new google.maps.Marker({
                         position: latLng,
@@ -167,9 +161,6 @@ function loadMap(map, txtinput) {
                         infowindow.open(map, this);
                         window.localStorage.setItem('eventNaam',this.bedrijf);
                         console.log(this.bedrijf);
-                    });
-                    google.maps.event.addListener(infowindow, 'click', function(){
-                        window.location=('Event_Detailpage.html');
                     });
                 }
 
